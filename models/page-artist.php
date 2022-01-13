3a7fd755a0d1f1ff919920a18ae1f0c9051fbcff
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2022-01-13 14:37:07 +0200

    Add role='status' && restore artist years format

--- models/page-artist.php
+++ models/page-artist.php
@@ -332,9 +332,13 @@ class PageArtist extends BaseModel {
             $item->permalink   = get_the_permalink( $item->ID );
             $additional_fields = get_fields( $item->ID );
 
-            $item->years  = ! empty( $additional_fields['death_year'] )
-                ? $additional_fields['death_year']
-                : $additional_fields['birth_year'];
+            if ( ! empty( $additional_fields['birth_year'] ) && ! empty( $additional_fields['death_year'] ) ) {
+                $item->years = $additional_fields['birth_year'] . ' - ' . $additional_fields['death_year'];
+            }
+            elseif ( ! empty( $additional_fields['birth_year'] ) ) {
+                $item->years = $additional_fields['birth_year'];
+            }
+
             $item->fields = $additional_fields;
 
             $item->link = [
