db53fbc3f00f38ecd543cbac1b92a6237cabbec8
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2022-05-12 14:27:07 +0300

    Use image src instead of id

--- models/page-artwork.php
+++ models/page-artwork.php
@@ -230,7 +230,12 @@ class PageArtwork extends PageArtist {
 
         return array_map( function ( $item ) use ( $artist_map ) {
             if ( has_post_thumbnail( $item->ID ) ) {
-                $item->image = get_post_thumbnail_id( $item->ID );
+                $item->image = get_the_post_thumbnail_url( $item->ID, 'medium' );
+
+                $image_id = get_post_thumbnail_id( $item->ID );
+                $image    = get_post( $image_id );
+
+                $item->image_alt = $image->post_excerpt ?? '';
             }
 
             $item->permalink = get_the_permalink( $item->ID );
